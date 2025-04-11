d371c9acb71a266820d561f3d47e3fe95d315b57
Sueda Kübra Gözler <2210238005@ogrenci.karabuk.edu.tr>
2025-04-11 18:37:59 +0300

    Sepet ekleme ve testler eklendi

---
+++ tests/tests_app.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Telefon' in response.data
+
+def test_add_to_cart(client):
+    response = client.get('/add/1')
+    assert response.status_code == 200
+    assert b'Telefon sepete eklendi' in response.data
